e7c3f042e528fcd22aace5499084123d2c3292c8
naveengitboi <naveenjangiti9@gmail.com>
2023-02-02 01:39:16 +0530

    api is not added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
+import './App.css'
 function App() {
+  const [data, setData] = useState('Hello')
+  const url = `https://movie-database-alternative.p.rapidapi.com/?s=Avengers%20Endgame&r=json&page=1`
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'ec14b9c38amsh9669f07d91a6a2ep1d850djsn63f2b94698f6',
+		'X-RapidAPI-Host': 'online-movie-database.p.rapidapi.com'
+	}
+};
+
+fetch('https://online-movie-database.p.rapidapi.com/auto-complete?q=game%20of%20thr', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="search" placeholder="search for a movie" />
+      <div className="container">
+        <div className="movieContainer">
+          <p className="year">2016</p>
+          <img src="" alt="" />
+          <div className="content">
+            <h1 className="movieName">Avengers</h1>
+            <p className="gener">Gener <span>movie</span></p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
